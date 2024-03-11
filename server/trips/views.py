ce007942f0bccd3fcf8e13dacd874249be2e2de6
felippenagy <felippenagy@outlook.com>
2024-03-11 16:32:47 -0400

    :rocket: adding tests to the project and setting up urls and views for trips

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView # new
+from .models import Trip
 
-from .serializers import LogInSerializer, UserSerializer # changed
+
+from .serializers import LogInSerializer, UserSerializer, TripSerializer # changed
 # Create your views here.
 class SignUpView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
@@ -11,3 +13,8 @@ class SignUpView(generics.CreateAPIView):
 
 class LogInView(TokenObtainPairView): # new
     serializer_class = LogInSerializer
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
